8650ea092acf678bdfb1d5e1ef82c2d568f38e78
rahulroyrdx001 <rahulroyrdx001@gmail.com>
2024-11-28 23:13:24 +0530

    validation added

--- basicForm/src/component/From.jsx
+++ basicForm/src/component/From.jsx
@@ -12,13 +12,37 @@ function From() {
     message: "",
   });
 
+  const [error, setError] = useState({});
+
+  const validate = (fromdata) => {
+    const newerror = {};
+    if (!fromdata.firstname) {
+      newerror.firstname = "first Name require";
+    }
+    if (!fromdata.lastname) {
+      newerror.lastname = "Last name require";
+    }
+    if (!fromdata.email) {
+      newerror.email = "email require";}
+    // } else if (!/\S+@\S+\.\S+/.test(fromdata.email)) {
+    //   newerror.email = "Valid email require";
+    // }
+    if (!fromdata.phone) {
+      newerror.phone = "Phone number require";
+    }
+    setError(newerror);
+    return newerror;
+  };
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFromdata({ ...fromdata, [name]: value });
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
+    const valdata = validate(fromdata);
+    if (Object.keys(valdata).length) return;
+    console.log(valdata);
     console.log(fromdata);
   };
 
@@ -32,6 +56,7 @@ function From() {
           name="firstname"
           onChange={handleChange}
         />
+        <p>{error.firstname}</p>
         <label htmlFor="lastname">Last Name*</label>
         <input
           type="text"
@@ -39,8 +64,10 @@ function From() {
           name="lastname"
           onChange={handleChange}
         />
+        <p>{error.lastname}</p>
         <label htmlFor="email">Email*</label>
         <input type="email" id="email" name="email" onChange={handleChange} />
+        <p>{error.email}</p>
         <label htmlFor="phone">Contact number*</label>
         <input type="tel" id="phone" name="phone" onChange={handleChange} />
         <input
@@ -50,6 +77,7 @@ function From() {
           value="male"
           onChange={handleChange}
         />
+        <p>{error.phone}</p>
         <label htmlFor="male">Male</label>
         <br />
         <input
